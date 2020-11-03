323dd15e3e90aac9f6ada6f2f0eddbbb67d0992a
dmytro.kukharuk <Di80977298968>
2020-11-03 19:01:27 +0200

    created structure and added @types to the app with help yarn add: - @types/react-redux - @types/react-router-dom

---
+++ src/App/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import logo from '../logo.svg';
+import styles from './App.module.scss';
+
+export default function App() {
+  return (
+      <BrowserRouter>
+        <div className={styles.App}>
+          <header className={styles.App_header}>
+            <img src={logo} className={styles.App_logo} alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className={styles.App_link}
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </BrowserRouter>
+  );
+
+}
